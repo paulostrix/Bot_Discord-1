c29e5f703e9d76a05c12b6836b93819c392486c5
Paulo Henrique <paulo.oliveira@estudante.ufla.br>
2020-02-16 12:45:13 -0300

    Estrutura inicial de comandos adicionado

--- bot.js
+++ bot.js
@@ -8,11 +8,26 @@ client.on("ready", () => {
 });
 
 client.on("guildCreate", guild => {
-    console.log("O bot entrou nos servidor:" + guild.name + guild.id + "população: " + guild.memberCount + " membros")
+    console.log("O bot entrou no servidor:" + guild.name + guild.id + "população: " + guild.memberCount + " membros")
+    client.user.setActivity(`Servindo ${client.guilds.size} servers`)
+})
+client.on("guildDelete", guild => {
+    console.log("O bot saiu no servidor:" + guild.name + guild.id + "população: " + guild.memberCount + " membros")
+    client.user.setActivity(`Servindo ${client.guilds.size} servers`)
 })
 
 client.on("message", async message => {
-   
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+    if(!message.content.startsWith(config.prefix)) return;
+
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const comando = args.shift().toLowerCase()
+    console.log(comando)
+    if(comando === "ping"){
+        const m = await message.channel.send("Ping?");
+        m.edit(`pong! A latência do bot é ${m.createdTimestamp - message.createdTimestamp} ms. A latência da API é ${Math.round(client.ping)}ms.`)
+    }
 })
 
 client.login(config.token)
